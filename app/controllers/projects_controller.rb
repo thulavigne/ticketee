6a5a78c90da11614138f54780f3826a7935b323b
Thu Quach <thuquach90@gmail.com>
2013-07-22 18:31:11 -0700

    Restrict access to project actions to admins only.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
 
+before_filter :authorize_admin!, :except => [:index, :show]
+before_filter :find_project, :only => [:show, :edit, :update, :destroy]
 before_filter :find_project, :only => [:show,
                                        :edit,
                                        :update,
@@ -47,12 +49,21 @@ before_filter :find_project, :only => [:show,
   end
 
   private
-    def find_project
-      @project = Project.find(params[:id])
-      rescue ActiveRecord::RecordNotFound
-      flash[:alert] = "The project you were looking" +
-                      " for could not be found."
-      redirect_to projects_path
+
+  def authorize_admin!
+    authenticate_user!
+    unless current_user.admin?
+      flash[:alert] = "You must be an admin to do that."
+      redirect_to root_path
     end
+  end
+
+  def find_project
+    @project = Project.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+    flash[:alert] = "The project you were looking" +
+                    " for could not be found."
+    redirect_to projects_path
+  end
 
 end
