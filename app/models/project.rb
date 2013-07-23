a4b7ab843bf70b5f9c9627d62610b67b8505c743
Thu Quach <thuquach90@gmail.com>
2013-07-23 10:44:17 -0700

    Don't show projects that a user doesn't have permission to see.

--- app/models/project.rb
+++ app/models/project.rb
@@ -8,6 +8,10 @@ class Project < ActiveRecord::Base
                                                 :user_id => user.id })
   end
 
+  def self.for(user)
+    user.admin? ? Project : Project.viewable_by(user)
+  end
+
   has_many :tickets, :dependent => :delete_all
 
 end
